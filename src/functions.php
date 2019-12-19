ccfe630e21e851a1cbdc71d3ac93fb900d713ac8
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-12-19 01:55:29 -0600

    added functions.php

---
+++ src/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+// connects to DB
+// returns the PDO connection
+function dbConnect() {
+  include_once('db-info.php');
+
+  try {
+    // connect to database
+    $pdo = new PDO("mysql:host=$host;dbname=$dbName",$user,$password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $pdo;
+
+  } catch(PDOexception $e) {
+      return 0;
+  }
+}
+
+?>
